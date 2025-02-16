a29f9f0e3ff62004d88818d1f784f4b4c32f4034
INOSUKE-SAMA <shahichirag10@gmail.com>
2025-02-16 16:05:24 +0545

    Day 3: File Colocation to Layouts

---
+++ app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function PrivateRoute(){
+    return <h1>You cannot view the in the browser.</h1>
+}
---
+++ app/auth/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+export default function ForgotPassword(){
+    return <h1>Forgot Password</h1>
+}
+
+//(auth) it say operation not permitted
