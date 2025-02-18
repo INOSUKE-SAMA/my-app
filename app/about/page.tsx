71be3d4875e540da890d1c451b91d1517f083d47
INOSUKE-SAMA <shahichirag10@gmail.com>
2025-02-18 21:12:09 +0545

    Day 3 : Title metadata and Link componets naviagtion

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata ={
+    title : "About CodeEvolution"
+}
+
 export default function about(){
     return <h1>About me</h1>
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata= {
+    title:"Blog"
+}
+
 export default function blog(){
     return <h1>My Blog</h1>
 }
